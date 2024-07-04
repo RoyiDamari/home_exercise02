b8d0e2c14e6fff54ff4883c0314ebba349dbd386
RoyiDamari <royi66@hotmail.com>
2024-07-04 21:33:07 +0300

    Add files via upload

---
+++ ProgramHomeExercise01.py
@@ -0,0 +1,17 @@
+# Exercise 1
+x: int = int(input("Pleas enter a number: "));
+
+# Printing the variable in a different line
+print(x);
+print(x);
+print(x);
+
+# Printing the variable in the same line
+print(x, x, x);
+
+# Exercise 2
+x: int = 777;
+y: int = 888;
+z: int = 999;
+result: int = x * y + z;
+print(result);
